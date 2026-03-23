cb9250ca7361a00bf3744277442e825bbc6862ab
monok8i <didkivskiyyura725@gmail.com>
2026-03-23 11:28:20 +0100

    feat: add key generation instructions to README and implement base64 decoding for JWT keys in config

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 
+
+
+
+## Generate public and private keys
+```bash
+# private
+openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+
+# public
+openssl rsa -pubout -in private.pem -out public.pem
+```
+
+### Convert .pem keys to base64 string:
+```bash
+base64 -w 0 private.pem
+base64 -w 0 public.pem
+```
+
+### Pass base64 string to .env file:
+```bash
+JWT_PUBLIC_KEY=...
+JWT_PRIVATE_KEY=...
+```
--- src/core/security/config.py
+++ src/core/security/config.py
@@ -1,5 +1,9 @@
 """JWT configuration."""
 
+import base64
+
+from pydantic import field_validator
+
 from src.core.config.env import BaseEnvConfig
 
 
@@ -11,3 +15,17 @@ class Config(BaseEnvConfig):
     JWT_ALGORITHM: str
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 5
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+
+    @field_validator("JWT_PRIVATE_KEY", mode="before")
+    @classmethod
+    def decode_private_key(cls, v: str) -> str:
+        """Decode the base64-encoded private key."""
+
+        return base64.b64decode(v).decode()
+
+    @field_validator("JWT_PUBLIC_KEY", mode="before")
+    @classmethod
+    def decode_public_key(cls, v: str) -> str:
+        """Decode the base64-encoded public key."""
+
+        return base64.b64decode(v).decode()
