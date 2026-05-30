daacac78f5e1b6666127641d19e977b4e36ef4e0
max <merzlyakov029@gmail.com>
2026-05-30 15:12:19 +0500

    refactor(api):  rename root path due to changed domain

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from .routes import auth
 
 # including routers to main api router
-router = APIRouter(prefix="/api")
-router.include_router(auth.router, prefix="/auth", tags=["authentication"])
+router = APIRouter(prefix="/auth")
+router.include_router(auth.router)
 
 __all__ = ["router"]
